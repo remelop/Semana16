74472cf28ee97802e412368ffebbd1ac22f51aef
remelop <re.melop@uea.edu.ec>
2024-03-27 15:11:24 -0500

    Entrega final

--- Lectura y Escritura.py
+++ Lectura y Escritura.py
@@ -2,8 +2,8 @@
 with open("my_notes.txt", "w") as archivo:
     # Escribir líneas de notas
     archivo.write("Ronny Eduardo.\n")
-    archivo.write("Melo Peñaherrera.\n")
-    archivo.write("35 años.\n\n")
+    archivo.write("Melo Penaherrera.\n")
+    archivo.write("35.\n\n")
 
 # Leer notas
 with open("my_notes.txt", "r") as archivo:
@@ -14,8 +14,8 @@ with open("my_notes.txt", "r") as archivo:
         linea = archivo.readline()
 
 # Escribir notas
-with open("my_notes.txt", "w") as archivo:
-    archivo.write("Quito, Pichincha, Norte.\n\n")
+with open("my_notes.txt", "a") as archivo:
+        archivo.write("Quito, Pichincha, Norte.\n\n")
 
 # Leer notas
 with open("my_notes.txt", "r") as archivo:
@@ -24,11 +24,4 @@ with open("my_notes.txt", "r") as archivo:
         print(linea.strip())
         linea = archivo.readline()
 
-with open("my_notes.txt", "w") as archivo:
-    archivo.write("Gracias.\n")
-with open("my_notes.txt", "r") as archivo:
-    linea = archivo.readline()
-    while linea:
-        print(linea.strip())
-        linea = archivo.readline()
 
